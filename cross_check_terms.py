cc9e940237d930181eb94df8e036dce5c9e14299
nayarh <69828676+nayarh@users.noreply.github.com>
2022-04-18 15:22:37 -0400

    Add files via upload

---
+++ cross_check_terms.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 15 15:07:28 2022
+
+@author: nayar
+"""
+
+#read csv 
+#read database text file 
+#run terms against database to save species and terms 
+
+with open("reptile_terms.csv", "r") as terms:
+    lines = terms.readlines(0)
+
+abb_terms=set()
+
+
+
+#putting abbreviations and terms in separate lists
+for t in lines:
+    
+    
+    abb_terms.add(t.split(',')[0])
+    abb_terms.add(t.split(',')[1])
+    
+abb_terms = list(abb_terms)
+
+
+
+#Sparse Matrix
+mat = [None] * len(abb_terms)
+
+#read from reptile database
+with open("reptile_database_2021_11.txt", encoding="utf_16") as f:
+    for line in f.readlines():
+        s = line.split("\t")
+        species_name = s[1] + " " + s[2]
+        for a in abb_terms:
+            
+            #cross checking 
+            if a in line:
+                if mat[abb_terms.index(a)] is not None:
+                    mat[abb_terms.index(a)].append(species_name)
+                else:
+                    mat[abb_terms.index(a)] = []
+                    mat[abb_terms.index(a)].append(species_name)
+                    
+                    
+                    
+abb_terms_name = input("What abbreviation or term?:")
+print(mat[abb_terms.index(abb_terms_name)])
